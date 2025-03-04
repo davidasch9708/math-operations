862501703138f92e04d6511602e2b9d5b9b0179a
davidasch9708 <davida_sch@gmx.com>
2025-03-04 19:33:26 +0000

    Add 1.rs

---
+++ src/1.rs
@@ -0,0 +1,9 @@
+// A simple function that takes two numbers and returns their sum
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+// A simple function that takes two numbers and returns their product
+fn multiply(x: i32, y: i32) -> i32 {
+    x * y
+}
