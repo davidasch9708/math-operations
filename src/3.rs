f3336d0998114db8398ba133ca625d02cfcae676
davidasch9708 <davida_sch@gmx.com>
2025-03-06 19:16:13 +0000

    Add 3.rs

---
+++ src/3.rs
@@ -0,0 +1,14 @@
+// Math operations repository
+use std::ops::{Add, Sub, Mul};
+
+pub fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+pub fn sub(x: i32, y: i32) -> i32 {
+    x - y
+}
+
+pub fn mul(x: i32, y: i32) -> i32 {
+    x * y
+}
